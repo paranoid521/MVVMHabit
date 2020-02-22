f0fef50355ef8bc3de15b5c99e722bed4d072cb5
andrew <mxjdocs@gmail.com>
2020-02-22 22:06:16 +0800

    更新BaseResponse元素

--- mvvmhabit/src/main/java/me/goldze/mvvmhabit/http/BaseResponse.java
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/http/BaseResponse.java
@@ -9,6 +9,25 @@ public class BaseResponse<T> {
     private String msg;
     private T data;
 
+    private String message;
+    private  T result;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
     public int getCode() {
         return code;
     }
